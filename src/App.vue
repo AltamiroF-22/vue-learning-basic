5aef29e40a9834f28278732df46d30fb52256458
Altamiro Junior <altamiroribeirodarocha@gmail.com>
2025-02-22 14:24:50 -0300

    DataBinding

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import FirstComponent from './components/FirstComponent.vue';
 import WithData from './components/WithData.vue'
 import WithPrimitiveReactiveData from './components/WithPrimitiveReactiveData.vue';
 import WithNonPrimitiveReactiveData from './components/WithNon-PrimitiveReactiveData.vue';
+import DataBiding from './components/DataBiding.vue';
 </script>
 
 <template>
   <header>
     Vue
   </header>
+  <DataBiding/>
   <WithNonPrimitiveReactiveData/>
   <WithPrimitiveReactiveData/>
   <WithData/>
---
+++ src/components/DataBiding.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const value = ref('vue is very cool');
+</script>
+
+<template>
+    <div class="component1">
+        <h1>Component with bind</h1>
+            <!-- <input type="text" v-bind:value="value"> -->
+            <!-- <input type="text" :value="value" v-on:input="(e)=> {value=e.target.value}"> -->
+
+            <!-- <input type="text" :value="value" @:input="(e)=> {value=e.target.value}"> -->
+            <input type="text" v-model="value">
+            <p>Input value: {{ value }}</p>
+    </div>
+</template>
+
+<style scoped></style>
