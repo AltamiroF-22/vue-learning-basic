698fa125eddd9a59f4d8fa3883fe39082ca144b2
Altamiro Junior <altamiroribeirodarocha@gmail.com>
2025-02-22 18:18:59 -0300

    Slots

---
+++ src/components/Slots.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+    <div class="component">
+        <h2>Component with slots</h2>
+        <button>
+            <slot name="icon"/> 
+            <slot name="text"> Button </slot> 
+    </button>
+    </div>
+</template>
