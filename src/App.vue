8609b6c8b3157f9db4f83622181be3c3a9b950f7
Altamiro Junior <altamiroribeirodarocha@gmail.com>
2025-02-21 21:37:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FirstComponent from './components/FirstComponent.vue';
+</script>
+
+<template>
+  <header>
+    Vue
+  </header>
+  <FirstComponent/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h1>Fist component</h1>
+  </section>
+</template>
+
+<style scoped>
+  section{
+    background-color: #111;
+    padding: 2em;
+    border-radius: 2em;
+    margin: 0.5em;
+    transition: .2s;
+    text-align: center;
+  }
+
+  section:hover{
+    background-color: #222;
+  }
+  h1{
+    color: aliceblue;
+    margin-bottom: 1em;
+  }
+</style>
