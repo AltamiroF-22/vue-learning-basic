7fa967bcd309b8a6b0cd20c3891f63c11c9c66ec
Altamiro Junior <altamiroribeirodarocha@gmail.com>
2025-02-21 23:26:40 -0300

    WithNon-PrimitiveReactiveData

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import FirstComponent from './components/FirstComponent.vue';
 import WithData from './components/WithData.vue'
 import WithPrimitiveReactiveData from './components/WithPrimitiveReactiveData.vue';
+import WithNonPrimitiveReactiveData from './components/WithNon-PrimitiveReactiveData.vue';
 </script>
 
 <template>
   <header>
     Vue
   </header>
+  <WithNonPrimitiveReactiveData/>
   <WithPrimitiveReactiveData/>
   <WithData/>
   <FirstComponent/>
---
+++ src/components/WithNon-PrimitiveReactiveData.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="component">
+        <h1>component with non-primitive reactive data</h1>
+        <p>Name:{{ person.name }} | Age: {{ person.age}}</p>
+        <button v-on:click="changeName()">change name</button>
+        <button @click="birthday()">birthday</button>
+
+    </div>
+</template>
+
+
+<!-- <script setup>
+    import {reactive} from 'vue'
+
+    const person = reactive({name:'junior', age: 22})
+
+    const birthday = () => {
+        person.age+= 1
+    }
+
+    const changeName = () => {
+     person.name = "junior rocha"
+    }
+</script> -->
+
+<script>
+export default {
+    data:function(){
+        return{
+            person:{
+                name:'Altamiro',
+                age: 22,
+            }
+        }
+        
+    },
+
+    methods:{
+        birthday: function(){
+            this.person.age += 1
+        },
+        changeName: function(){
+            this.person.name = 'Altamiro ferrari'
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+button{
+    padding: 1em;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
